10fac2f710a5af05ed96be56c8608ca7ffa68ac0
Prajakta <prajaktawadhi3854@gmail.com>
2025-02-03 13:03:27 +0530

    model classes created

---
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/model/SeatType.java
@@ -0,0 +1,29 @@
+package com.bookmymovie.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "seat_types")
+public class SeatType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long typeId;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
+    private String description;
+
+    private Double basePrice;
+}
