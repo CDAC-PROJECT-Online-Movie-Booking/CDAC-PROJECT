26fd2863d65063bc0e0fd53d72128b7797130252
Suraj Raut <spraut857@gmail.com>
2025-02-04 12:05:27 +0530

    User controller

--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/controllers/UserController.java
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.bookmymovie.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bookmymovie.model.User;
 import com.bookmymovie.service.UserService;
 
 @RestController
@@ -18,5 +25,17 @@ public class UserController {
 		
 	}
 	
-	
+	@GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    @GetMapping
+//    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+
+
 }
--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/service/UserServiceImpl.java
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/service/UserServiceImpl.java
@@ -1,5 +1,30 @@
 package com.bookmymovie.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bookmymovie.exception.ResourceNotFoundException;
+import com.bookmymovie.model.User;
+import com.bookmymovie.repository.UserRepository;
+
+
 public class UserServiceImpl implements UserService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+				
+	}
+
+	@Override
+	public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
 
 }
