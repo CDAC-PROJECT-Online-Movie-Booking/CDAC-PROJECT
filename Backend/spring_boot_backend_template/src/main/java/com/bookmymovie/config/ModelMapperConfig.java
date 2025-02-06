e5d1d65b6fb04c23ce09a0b8c8479625ae3e4224
Suraj Raut <spraut857@gmail.com>
2025-02-06 09:18:53 +0530

    Added correction

--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/config/ModelMapperConfig.java
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/config/ModelMapperConfig.java
@@ -1,5 +1,45 @@
 package com.bookmymovie.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.bookmymovie.dto.BookingResponse;
+import com.bookmymovie.dto.ShowtimeResponse;
+import com.bookmymovie.entity.Booking;
+import com.bookmymovie.entity.Showtime;
+
+@Configuration
 public class ModelMapperConfig {
 
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+            .setMatchingStrategy(MatchingStrategies.STRICT)
+            .setSkipNullEnabled(true);
+
+        // Custom mappings
+        configureBookingMappings(mapper);
+        configureShowtimeMappings(mapper);
+        
+        return mapper;
+    }
+
+    private void configureBookingMappings(ModelMapper mapper) {
+        mapper.typeMap(Booking.class, BookingResponse.class)
+            .addMappings(m -> {
+                m.map(src -> src.getUser().getName(), BookingResponse::setUserName);
+                m.map(src -> src.getShowtime().getDateTime(), BookingResponse::setShowtime);
+            });
+    }
+
+    private void configureShowtimeMappings(ModelMapper mapper) {
+        mapper.typeMap(Showtime.class, ShowtimeResponse.class)
+            .addMappings(m -> {
+                m.map(src -> src.getMovie().getTitle(), ShowtimeResponse::setMovieTitle);
+                m.map(src -> src.getScreen().getTheater().getName(), ShowtimeResponse::setTheaterName);
+            });
+    }
 }
+
