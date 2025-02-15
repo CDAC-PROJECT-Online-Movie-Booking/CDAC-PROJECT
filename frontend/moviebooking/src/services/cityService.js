ee3904f701f53352c510f5fb4206a4d03ed44d3d
Manas-2210 <cdac2210@gmail.com>
2025-02-15 12:19:42 +0530

    Deleting Old prototype Frontend

--- frontend/moviebooking/src/services/cityService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getCities = async () => {
-  const response = await api.get("/cities");
-  return response.data;
-};
--- frontend/moviebooking/src/services/reportService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getRevenueByMovie = async () => {
-  const response = await api.get("/reports/revenue-by-movie");
-  return response.data;
-};
