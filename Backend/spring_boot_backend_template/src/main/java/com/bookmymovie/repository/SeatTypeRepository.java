4cc669ce5b62876e9142330d73ee9833d9706bc7
Suraj Raut <spraut857@gmail.com>
2025-02-17 18:15:41 +0530

    changes made

--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/repository/SeatTypeRepository.java
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/repository/SeatTypeRepository.java
@@ -1,11 +1,11 @@
 package com.bookmymovie.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bookmymovie.models.SeatType;
 
-public interface SeatTypeRepository extends JpaRepository<SeatType, Long> {
-    Optional<SeatType> findByName(String name);
+@Repository
+public interface SeatTypeRepository extends JpaRepository<SeatType, Integer> {
+
 }
