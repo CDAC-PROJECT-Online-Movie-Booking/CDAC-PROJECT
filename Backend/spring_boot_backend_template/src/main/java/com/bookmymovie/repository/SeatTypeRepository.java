abd5738355a2b45cb2867eedbabc360db852ee7d
Manas-2210 <cdac2210@gmail.com>
2025-02-17 18:38:17 +0530

    Adding SeatTypeService

--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/repository/SeatTypeRepository.java
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/repository/SeatTypeRepository.java
@@ -8,4 +8,6 @@ import com.bookmymovie.model.SeatType;
 
 public interface SeatTypeRepository extends JpaRepository<SeatType, Long> {
     Optional<SeatType> findByName(String name);
+    
+    Optional<SeatType> findById(int id);
 }
