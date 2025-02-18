68ac9d60c1368aec2a92f3a2df74363bddd5b90f
Bhushan Patil <bhushanlaxmanpatil3236@gmail.com>
2025-02-18 14:41:49 +0530

    changed Movie Controller

--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/controllers/MovieController.java
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/controllers/MovieController.java
@@ -1,46 +1,46 @@
 package com.bookmymovie.controllers;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.bookmymovie.dto.MovieRequest;
-import com.bookmymovie.dto.MovieResponse;
 import com.bookmymovie.models.Movie;
 import com.bookmymovie.service.MovieService;
 
-import lombok.RequiredArgsConstructor;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/movies")
-@RequiredArgsConstructor
+
 public class MovieController {
-	
-	 private final MovieService movieService;
+	@Autowired private MovieService mService;
 
-	    @GetMapping
-	    public ResponseEntity<List<MovieResponse>> getAllMovies() {
-	        return ResponseEntity.ok(movieService.getAllMovies());
-	    }
+    @PostMapping
+    public ResponseEntity<?> saveMovie(Movie movie,@RequestPart(required = false) MultipartFile photo){
+        mService.save(movie,photo);
+        return ResponseEntity.ok().body("Movie saved successfully");
+    }
 
-	    @GetMapping("/{movieId}")
-	    public ResponseEntity<MovieResponse> getMovie(@PathVariable Long movieId) {
-	        return ResponseEntity.ok(movieService.getMovieById(movieId));
-	    }
+    @GetMapping
+    public ResponseEntity<?> listall(){
+        return ResponseEntity.ok(mService.listall());
+    }
 
-	    @PostMapping
-	    @PreAuthorize("hasRole('ADMIN')")
-	    public ResponseEntity<?> addMovie(@RequestBody MovieRequest movie) {
-	        return ResponseEntity.status(HttpStatus.CREATED).body(movieService.addMovie(movie));
-	    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteMovie(@PathVariable("id") int id){
+        mService.deleteMovie(id);
+        return ResponseEntity.ok("Movie deleted successfully");
+    }
 
-	    @GetMapping("/search")
-	    public ResponseEntity<List<MovieResponse>> searchMovies(@RequestParam String query) {
-	        return ResponseEntity.ok(movieService.searchMovies(query));
-	        		
-	    }
-
+    @GetMapping("{id}")
+    public ResponseEntity<?> findDetails(@PathVariable("id") int id){
+        return ResponseEntity.ok().body(mService.findById(id));
+    }
 }
