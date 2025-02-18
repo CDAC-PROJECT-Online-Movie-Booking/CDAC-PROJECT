c9b21cbd8de244b10048863b5532421cbf991ef5
Prajakta <prajaktawadhi3854@gmail.com>
2025-02-18 15:05:30 +0530

    Custom exceptions added

--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.bookmymovie.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
