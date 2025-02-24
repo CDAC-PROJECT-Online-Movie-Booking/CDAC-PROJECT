909aa83251bebc07155f06252e7707e1e6cebf84
Bhushan Patil <bhushanlaxmanpatil3236@gmail.com>
2025-02-25 02:39:07 +0530

    changes in Booking repository

--- Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/repository/BookingRepository.java
+++ Backend/spring_boot_backend_template/src/main/java/com/bookmymovie/repository/BookingRepository.java
@@ -2,17 +2,27 @@ package com.bookmymovie.repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bookmymovie.models.Booking;
 import com.bookmymovie.models.User;
+
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 	
 	List<Booking> findByUser(User user);
 	List<Booking> findByShowShowIdAndShowDate(int showid,LocalDate date);
+	
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT b FROM Booking b WHERE b.id = :id")
+    Optional<Booking> findByIdWithLock(@Param("id") int id);
 
 }
